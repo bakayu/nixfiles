3e5dc9c1c403841ee3e3962d27c1794bc4246cd9
bakayu <ayu.0067abc@gmail.com>
2024-11-13 16:35:01 +0530

    updated vscode.nix

--- vscode.nix
+++ vscode.nix
@@ -3,7 +3,7 @@
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "vscode"
   ];
-  environment.systemPackages = with pkgs; [ 
+  environment.systemPackages = with pkgs; [
     vscode
   ];
 }
